437ac6db4d277a36bcfb116690c3e6d40e832827
Kravets <106190014+Kravetsin@users.noreply.github.com>
2022-08-26 21:46:54 +0200

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,1 +1,20 @@
-alert("Hello world!")
+"use strict";
+
+const MY_BIRTHDAY = "26.04.1999";
+// alert(`my birthday is ${MY_BIRTHDAY}`);
+// let age = prompt('How old are you?', 18);
+
+
+const time = new Date().getHours();
+
+let greeting;
+
+if (time < 10) {
+  greeting = "Good morning";
+} else if (time < 20) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
+
+document.createElement("dataInfo") = greeting;
