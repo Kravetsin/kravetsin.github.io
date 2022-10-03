6d5869969d2c043fcce0f9b98b387392ac274aea
Kravetsin <kravets.inside@gmail.com>
2022-10-03 20:13:38 +0200

    Calculator Changes

--- calculator.js
+++ calculator.js
@@ -3,7 +3,7 @@
 let op; 
 let result_g;
 
-function func() {
+function accept_g() {
 
   switch (op) {
     case '300':
@@ -37,14 +37,24 @@ let rrow = [104, 97, 180];
 let nrow = [13, 7, 12];
 let mrow = [8, 8, 15];
 
-function accept(){
+function accept_ks(){
 
   let num1 = Number(document.getElementById("num1").value);
   let num2 = Number(document.getElementById("num2").value);
   let num3 = Number(document.getElementById("num3").value);
   let num4 = Number(document.getElementById("num4").value);
 
-  let result_all = (num1 * rrow[arr]) + (num2 * nrow[arr]) + (num3 * mrow[arr]) + num4;
+  let result_all = ((num1 * rrow[arr]) + (num2 * nrow[arr]) + (num3 * mrow[arr]) + num4) + ' KS(штук)';
 
   document.getElementById('result_all').innerHTML = result_all;
+}
+
+let score = 0;
+let updating = 1;
+
+function level_up(){
+
+  score = score + updating;
+
+  document.getElementById('score').innerHTML = score
 }
