9e329c73536e374df0e301e9714b2cd9ae4b7bd3
Kravetsin <kravets.inside@gmail.com>
2022-10-09 16:04:42 +0200

    New features

---
+++ test.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// Функция для слайдера
+function changeValue(event) {
+    let value = event.target.value;
+    let output = document.getElementById('output');
+    output.value = value;
+}
